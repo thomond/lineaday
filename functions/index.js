4b62bb4bf41f8254848bdd31180722c8bdc29bd2
Jess Robins <j.robins@thelevelup.com>
2018-06-16 23:36:31 -0400

    actually sent one

--- functions/index.js
+++ functions/index.js
@@ -1,34 +1,25 @@
 const functions = require('firebase-functions')
 const admin = require('firebase-admin')
 
-admin.initializeApp(functions.config().firebase)
+admin.initializeApp()
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
 exports.sendNotifications = functions.https.onRequest((req, res) => {
-  const hour = req.body.hour
+  const { hour } = req.body
 
-  if (hour !== 0 && !hour) {
-    return res.status(422).send('Hour is required')
+  if (typeof hour !== 'number') {
+    return res.status(422).send('Hour is required to be a number.')
   }
 
   return getUsersForHour(hour)
-    .then((snapshot) => {
-      let notificationPromises = []
-      snapshot.forEach((snapshot) => {
-        notificationPromises.push(sendNotificationToUser(snapshot))
-      })
-
-      return Promise.all(notificationPromises)
-    })
-    .then((response) => {
-      console.log('response is ', response)
+    .then(sendNotificationsToAllUsers)
+    .then(() => {
       res.end()
       return null
     })
-    .catch(console.log)
-
+    .catch(err => {
+      console.log(err)
+      res.status(500).send(err)
+    })
 })
 
 function getUsersForHour(hour) {
@@ -38,19 +29,52 @@ function getUsersForHour(hour) {
     .get()
 }
 
+function sendNotificationsToAllUsers(snapshot) {
+  let notificationPromises = []
+  snapshot.forEach((snapshot) => {
+    notificationPromises.push(sendNotificationToUser(snapshot))
+  })
+
+  return Promise.all(notificationPromises)
+}
+
 function sendNotificationToUser(snapshot) {
   const user = snapshot.data()
   const tokens = Object.keys(user.messagingTokens || {})
   const payload = {
     notification: {
-      title: 'Remember!',
-      body: "It's time to write in your journal",
+      title: 'tinythoughts',
+      body: "It's time to write in your journal today!",
+      icon: 'https://lineaday-11542.firebaseapp.com/notebook.png',
+      click_action: 'https://tinythoughts.me'
     }
   }
 
   if (tokens.length) {
     return admin.messaging().sendToDevice(tokens, payload)
+      .then(response => {
+          // For each message check if there was an error.
+          const tokensToRemovePromises = []
+          response.results.forEach((result, index) => {
+            const error = result.error
+            if (error) {
+              console.log('Failure sending notification to', tokens[index], error)
+              // Cleanup the tokens who are not registered anymore.
+              if (error.code === 'messaging/invalid-registration-token' ||
+                  error.code === 'messaging/registration-token-not-registered') {
+                tokensToRemovePromises.push(removeToken(snapshot.id, tokens[index]))
+              }
+            }
+          })
+          return Promise.all(tokensToRemovePromises)
+      })
   }
 
   return Promise.resolve()
+}
+
+function removeToken(userId, token) {
+  return admin.firestore().collection('users').doc(userId).update({
+    ['messagingTokens.' + token]: admin.firestore.FieldValue.delete()
+  })
 }
