7fe73c285b5a01e7de8178e752fcea4562a1d539
Jess Robins <j.robins@thelevelup.com>
2018-06-19 12:12:47 -0400

    update firebase config and add functions just in case

--- functions/index.js
+++ functions/index.js
@@ -3,6 +3,7 @@ const admin = require('firebase-admin')
 const moment = require('moment')
 const secureCompare = require('secure-compare')
 const uuid = require('uuid/v4')
+const CryptoJS = require('crypto-js')
 const cors = require('cors')({
   origin: 'https://tinythoughts.me'
 })
@@ -113,3 +114,65 @@ exports.setEncryptionKey = functions.https.onRequest((req, res) => {
       })
   })
 })
+
+exports.addEncryptionKeyToUsers = functions.https.onRequest((req, res) => {
+  admin.auth().listUsers()
+    .then((result) => {
+      const promises = []
+      result.users.forEach(user => {
+        const encryptionKey = uuid()
+        if (!user.customClaims || !user.customClaims.encryptionKey) {
+          promises.push(admin.auth().setCustomUserClaims(user.uid, { encryptionKey }))
+        }
+      })
+
+      return Promise.all(promises)
+    })
+    .then(() => {
+      return res.end()
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).send(err)
+    })
+})
+
+exports.encryptTextForUser = functions.https.onRequest((req, res) => {
+  const { userId } = req.body
+  let encryptionKey
+
+  admin.auth().getUser(userId)
+    .then(userRecord => {
+      encryptionKey = userRecord.customClaims.encryptionKey
+
+      return admin.firestore()
+        .collection('users')
+        .doc(userId)
+        .collection('lines').get()
+    })
+    .then(snapshot => {
+      let promises = []
+
+      snapshot.forEach(doc => {
+        const text = doc.data().text
+        const encryptedText = CryptoJS.AES.encrypt(text, encryptionKey).toString()
+        const promise = admin.firestore()
+          .collection('users')
+          .doc(userId)
+          .collection('lines')
+          .doc(doc.id)
+          .set({ text: encryptedText }, { merge: true })
+        promises.push(promise)
+      })
+
+      return Promise.all(promises)
+    })
+    .then(() => {
+      return res.end()
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).send(err)
+    })
+})
+
