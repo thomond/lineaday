4b42a22fe538624afc78c114412a1db10e710935
Jess Robins <j.robins@thelevelup.com>
2018-06-19 12:40:07 -0400

    maybe display toast?

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-console */
 
 import { register } from 'register-service-worker';
+import { Toast } from 'buefy'
 
 if (process.env.NODE_ENV === 'production') {
   register(`${process.env.BASE_URL}service-worker.js`, {
@@ -13,6 +14,11 @@ if (process.env.NODE_ENV === 'production') {
     },
     updated() {
       console.log('New content is available; please refresh.');
+      Toast.open({
+        duration: 600000,
+        message: 'New content is available; please refresh.',
+        position: 'is-bottom',
+      })
     },
     offline() {
       console.log('No internet connection found. App is running in offline mode.');
