8e212b88f880ef6f0d67530c662bc9a22ef70f7c
Jess Robins <j.robins@thelevelup.com>
2018-12-11 09:39:07 -0500

    i don't know what this is but i want to save it

--- functions/sendNotifications.js
+++ functions/sendNotifications.js
@@ -1,18 +1,21 @@
 const functions = require('firebase-functions')
 const secureCompare = require('secure-compare')
 const moment = require('moment')
+const Expo = require('expo-server-sdk')
 const admin = require('./init')
+
+const expo = new Expo()
 
 const sendNotifications = functions.https.onRequest((req, res) => {
   const key = (req.get('Authorization') || '').split('Basic ')[1]
   const hour = moment().utc().hour()
 
-  if (!secureCompare(key, functions.config().cron.key)) {
-    console.log('The key provided in the request does not match the key set in the environment.')
-    res.status(403).send('Security key does not match. Make sure your "key" URL query parameter matches the ' +
-        'cron.key environment variable.')
-    return null
-  }
+  // if (!secureCompare(key, functions.config().cron.key)) {
+  //   console.log('The key provided in the request does not match the key set in the environment.')
+  //   res.status(403).send('Security key does not match. Make sure your "key" URL query parameter matches the ' +
+  //       'cron.key environment variable.')
+  //   return null
+  // }
 
   console.log('Checking for notifications for hour ', hour)
 
@@ -23,7 +26,7 @@ const sendNotifications = functions.https.onRequest((req, res) => {
       return null
     })
     .catch(err => {
-      console.log(err)
+      console.log('these errors', err)
       res.status(500).send(err)
     })
 })
@@ -45,23 +48,92 @@ function getPrompt() {
 
 function sendNotificationsToAllUsers([userSnapshot, promptSnapshot]) {
   let notificationPromises = []
+  let expoMessages = []
 
   const prompts = []
   promptSnapshot.forEach(snapshot => {
     prompts.push(snapshot.data())
   })
 
+  const prompt = prompts.length ? prompts[0].prompt : "It's time to write in your journal today!"
+
   userSnapshot.forEach((snapshot) => {
-    notificationPromises.push(sendNotificationToUser(snapshot, prompts))
+    notificationPromises.push(sendNotificationToUser(snapshot, prompt))
+    expoMessages.push(...getExpoMessagesForUser(snapshot, prompt))
   })
 
-  return Promise.all(notificationPromises)
+  const expoNotifications = handleExpoNotifications(expoMessages)
+
+  return Promise.all([...notificationPromises, ...expoNotifications])
 }
 
-function sendNotificationToUser(snapshot, prompts) {
-  const prompt = prompts.length ? prompts[0].prompt : "It's time to write in your journal today!"
+function handleExpoNotifications(messages) {
+  const chunks = expo.chunkPushNotifications(messages)
+  const tickets = []
+
+  return chunks.map(chunk => {
+    return expo.sendPushNotificationsAsync(chunk)
+      .then(ticketChunk => {
+        console.log('ticket chunk: ', ticketChunk)
+        tickets.push(...ticketChunk)
+        return handleExpoResponses(tickets)
+      })
+      .catch((errors) => {
+        console.log('what huh is it this?')
+      })
+  })
+}
+
+function handleExpoResponses(tickets) {
+  let receiptIds = []
+  tickets.forEach(ticket => {
+    if (ticket.id) {
+      receiptIds.push(ticket.id)
+    }
+  })
+
+  const receiptIdChunks = expo.chunkPushNotificationReceiptIds(receiptIds)
+
+  return receiptIdChunks.map(chunk => {
+    return expo.getPushNotificationReceiptsAsync(chunk)
+      .then(receipts => {
+        console.log(receipts)
+        receipts.forEach(receipt => {
+          if (receipt.status === 'ok') {
+            continue;
+          } else if (receipt.status === 'error') {
+            console.error(`There was an error sending a notification: ${receipt.message}`);
+            if (receipt.details && receipt.details.error) {
+              console.error(`The error code is ${receipt.details.error}`);
+            }
+          }
+        })
+      })
+  })
+}
+
+function getExpoMessagesForUser(snapshot, prompt) {
+  const user = snapshot.data()
+  const expoTokens = Object.keys(user.expoTokens || {})
+
+  return expoTokens.map(token => {
+    if (!Expo.isExpoPushToken(token)) {
+      console.error(`Push token ${token} is not a valid Expo push token`)
+    }
+
+    return {
+      to: token,
+      sound: 'default',
+      body: prompt,
+    }
+  })
+}
+
+function sendNotificationToUser(snapshot, prompt) {
   const user = snapshot.data()
   const tokens = Object.keys(user.messagingTokens || {})
+
+
   const payload = {
     notification: {
       title: 'tinythoughts',
@@ -70,6 +142,7 @@ function sendNotificationToUser(snapshot, prompts) {
       click_action: 'https://tinythoughts.me'
     }
   }
+
 
   if (tokens.length) {
     return admin.messaging().sendToDevice(tokens, payload)
